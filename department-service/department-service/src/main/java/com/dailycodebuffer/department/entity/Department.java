5875b706af64729417b631782732de2c88beb54c
webdesigner001 <71431181+webdesigner001@users.noreply.github.com>
2023-02-21 18:11:26 +0530

    Add files via upload

---
+++ department-service/department-service/src/main/java/com/dailycodebuffer/department/entity/Department.java
@@ -0,0 +1,22 @@
+package com.dailycodebuffer.department.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Department {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long departmentId;
+    private String departmentName;
+    private String departmentAddress;
+    private String departmentCode;
+}
---
+++ user-service/user-service/src/main/java/com/dailycodebuffer/user/entity/User.java
@@ -0,0 +1,23 @@
+package com.dailycodebuffer.user.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long userId;
+    private String firstName;
+    private String lastName;
+    private String email;
+    private long departmentId;
+}
